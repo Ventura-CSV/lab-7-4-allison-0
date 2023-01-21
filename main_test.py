d178daefe1698c9d017117773a949af44dcc9e03
Kyu Lee <kwooler@gmail.com>
2023-01-21 14:20:29 -0800

    L74

--- main_test.py
+++ main_test.py
@@ -8,8 +8,8 @@ def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
     # datastr = '90\n10\n50\n40\n30'
-    datastr = '90 10 50 40 30'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = '90 10 50 40 30'
+    # sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
@@ -17,32 +17,23 @@ def test_main_1():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    regex_string = r'[\w,\W]*90'
-    regex_string += r'[\w,\W]*50'
+    l1 = lines[0].strip('[').rstrip(']')
+    l1 = list(map(int, l1.split(',')))
+    print(l1)
+
+    l2 = lines[1].strip('[').rstrip(']')
+    l2 = list(map(int, l2.split(',')))
+    print(l2)
+
+    regex_string = ''
+    for i in range(len(l1)):
+        regex_string += r'[\w,\W]*' + str(l1[i]+l2[i])
     regex_string += r'[\w,\W]*'
+
+    # regex_string = r'[\w,\W]*90'
+    # regex_string += r'[\w,\W]*50'
+    # regex_string += r'[\w,\W]*'
     print(regex_string)
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
-
-
-def test_main_2():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = '90\n10\n50\n40\n30'
-    datastr = '1 2 3 4 5'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    regex_string = r'[\w,\W]*4'
-    regex_string += r'[\w,\W]*5'
-    regex_string += r'[\w,\W]*'
-    print(regex_string)
-    res = re.search(regex_string, lines[0])
+    res = re.search(regex_string, lines[2])
     assert res != None
     print(res.group())
