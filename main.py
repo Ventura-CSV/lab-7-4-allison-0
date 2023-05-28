cb3dde62148e1acc153f1d2068f4f902e154d70b
Kyu Lee <kwooler@gmail.com>
2023-05-28 16:48:12 -0700

    L74

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+
+def getInput():
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+
+def listSum(list1, list2):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    list1 = getInput()
+    list2 = getInput()
+    ret = listSum(list1, list2)
+    print(f'The result values are: {ret}')
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -4,36 +4,43 @@ import sys
 import re
 
 
-def test_main_1():
+def test_main_0():
     captureOut = io.StringIO()
     sys.stdout = captureOut
     # datastr = '90\n10\n50\n40\n30'
-    # datastr = '90 10 50 40 30'
-    # sys.stdin = io.StringIO(datastr)
+    datastr = '1 2 3\n 4 5 6\n'
+    sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    list1 = main.getInput()
+    list2 = main.getInput()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    l1 = lines[0].strip('[').rstrip(']')
-    l1 = list(map(int, l1.split(',')))
-    print(l1)
+    print(f'The inpust list 1: \t{list1}')
+    print(f'The inpust list 2: \t{list2}')
 
-    l2 = lines[1].strip('[').rstrip(']')
-    l2 = list(map(int, l2.split(',')))
-    print(l2)
+    ret = main.listSum(list1, list2)
+    print(f'Your retrun value is:\t {ret}')
 
-    regex_string = ''
-    for i in range(len(l1)):
-        regex_string += r'[\w,\W]*' + str(l1[i]+l2[i])
-    regex_string += r'[\w,\W]*'
 
-    # regex_string = r'[\w,\W]*90'
-    # regex_string += r'[\w,\W]*50'
-    # regex_string += r'[\w,\W]*'
-    print(regex_string)
-    res = re.search(regex_string, lines[2])
-    assert res != None
-    print(res.group())
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = '90\n10\n50\n40\n30'
+    datastr = '1 3 4 5 2 7 8 1 2 5\n 7 8 1 2 5 1 3 5 4 2\n'
+    sys.stdin = io.StringIO(datastr)
+
+    list1 = main.getInput()
+    list2 = main.getInput()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    print(f'The inpust list 1:\t {list1}')
+    print(f'The inpust list 2:\t {list2}')
+
+    ret = main.listSum(list1, list2)
+    print(f'Your retrun value is:\t {ret}')
